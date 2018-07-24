3a7e1b51952c02b019531e9bf81df0de6339203d
angad <angadsharma1016@gmail.com>
2018-07-25 01:31:35 +0530

    form

--- static/js/script1.js
+++ static/js/script1.js
@@ -1,4 +1,14 @@
-$(".closecard").on("click",(e)=>{
-    e.preventDefault();
-    $(".mycard").html("HELLO")
+$(document).ready(()=>{
+    $("#input_body").hide();
+    $('select').formSelect();
+    // $('#textarea1').val('New Text');
+    // M.textareaAutoResize($('#textarea1'));
+    M.updateTextFields();
+
+    $(".closecard").on("click",(e)=>{
+        e.preventDefault();
+        $(".mycard").html("");
+        $("#input_body").show();
+        $(".ip").val('');
+    });
 });
